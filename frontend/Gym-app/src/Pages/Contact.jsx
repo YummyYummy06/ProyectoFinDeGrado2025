6511e64ee801edc0c2d85e7300b0148a74cafd44
YummyYummy06 <Letiel52@gmail.com>
2025-12-14 00:24:50 +0100

    Se ha creado pagina contacto

--- frontend/Gym-app/src/Pages/Contact.jsx
+++ frontend/Gym-app/src/Pages/Contact.jsx
@@ -44,7 +44,25 @@ function Contact() {
             </ul>
           </menu>
         </header>
-        <div className="contenedor-contacto"></div>
+        <div className="contenedor-contacto">
+          <div className="columnaContacto">
+            <iframe
+              className="mapa"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d113989.34151499256!2d-4.947392984929788!3d36.500960512875025!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd72d809904dabdf%3A0xe6c9db907b5ecab!2sMarbella%2C%20M%C3%A1laga!5e1!3m2!1ses!2ses!4v1765666012519!5m2!1ses!2ses"
+              allowFullScreen=""
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            ></iframe>
+          </div>
+          <div className="columnaContacto-1">
+            <h3 className="info-contacto">WHATSAPP</h3>
+            <a className="info-link">+34 633 456 789</a>
+            <h3 className="info-contacto">EMAIL</h3>
+            <a className="info-link">TheclubBoxingStudioMarbella@gmail.com</a>
+            <h3 className="info-contacto">ADRESS</h3>
+            <a className="info-link">Av. Fontanilla, 3, Marbella, CP 29601</a>
+          </div>
+        </div>
       </div>
     </>
   );
