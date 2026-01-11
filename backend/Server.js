2aa4431df3e6c24ccf078702ca1519f69f1a5131
YummyYummy06 <Letiel52@gmail.com>
2026-01-11 22:28:10 +0100

    cambio de origen en cors

--- backend/Server.js
+++ backend/Server.js
@@ -25,7 +25,7 @@ const PORT = process.env.PORT || 3000;
 
 //Middleware
 app.use(cors({
-    origin: "http://localhost:3001",
+    origin: "https://theclub-boxingstudio.vercel.app/",
     credentials: true,
 }))
 app.use(express.json());
